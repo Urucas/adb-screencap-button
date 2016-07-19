6bfa6ee480d161f1477273890c219dce9af2668d
vrunoa <vruno.alassia@gmail.com>
2016-07-19 19:20:43 -0300

    add multiple device support

--- public/js/app.js
+++ public/js/app.js
@@ -2,16 +2,17 @@ var electron = require('electron');
 var img = document.getElementById("preview");
 var btt = document.getElementById("takeBtt");
 var closeBtt = document.getElementById("closeBtt");
-console.log(closeBtt);
 var preloader = document.getElementById("preloader");
+var select = document.getElementById("devices");
 var ipcRenderer = electron.ipcRenderer;
+var devices;
 function takeScreenshot() {
   preview.removeAttribute("src");
   preloader.style.visibility = "visible";
   btt.setAttribute("disabled", "disabled");
   var audio = new Audio('sound/shutter.mp3');
   audio.play();
-  ipcRenderer.send('screencap', {});
+  ipcRenderer.send('screencap', {id:select.value});
 }
 function close() {
   ipcRenderer.send("close");
@@ -23,3 +24,14 @@ ipcRenderer.on("screencap-ready", function(ev, msg){
   preview.src = msg;
 });
 closeBtt.onclick = close;
+ipcRenderer.send("get-devices");
+ipcRenderer.on("devices", function(ev, data) {
+  devices = data;
+  for(i in data) {
+    var device = data[i];
+    var option = document.createElement("option");
+    option.value = device.id;
+    option.innerText = device.model + " - " + device.id;
+    select.appendChild(option);
+  }
+})
